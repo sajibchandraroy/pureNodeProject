6c1430d4e63df929b8da80f8a07efb643cee216f
sajibchandraroy <66843005+sajibchandraroy@users.noreply.github.com>
2021-07-18 18:10:36 +0600

    update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // dependencies
 const http = require('http');
+require('dotenv').config()
 const { handleReqRes } = require('./helpers/handleReqRes');
 
 
@@ -8,13 +9,13 @@ const app = {};
 
 // configuration
 app.config = {
-    port: 5000 ,
+    port: 5000 || process.env.PORT,
 };
 
 // create server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port || process.env.PORT, () => {
+    server.listen(app.config.port, () => {
         console.log(`listening to port ${app.config.port}`);
     });
 };
